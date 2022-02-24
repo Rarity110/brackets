6b2d8d09e9eceae414a28d160587a60220bef275
Rarity110 <rarity110@mail.ru>
2022-02-24 22:38:53 +0300

    feat: create function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let x = true;
+  let tmp = str;
+    
+  while (x === true) {
+    x = false;
+    for (let i = 0; i < tmp.length - 1; i++) {
+      for (let j = 0; j < bracketsConfig.length; j++) {
+        if (tmp[i] === bracketsConfig[j][0] && tmp[i + 1] === bracketsConfig[j][1]) {
+          tmp = tmp.substr(0, i) + tmp.substr(i + 2);
+          x = true; 
+        }
+      }
+    }
+  }
+
+  if (tmp.length === 0) {
+    return true;
+  } else return false;
 }
